5cb5c0f70d587a1cba708cbc919f24248e16b342
ashvaneetk <123331072+ashvaneetk@users.noreply.github.com>
2023-04-17 13:45:19 +0530

    Create DesignHalfPyramidUsingNumbersWhereNumber=NumberOfTheRow.cpp
    This program takes input as row and Display Half Pyramid where using Number, where number=Number of the row

---
+++ DesignHalfPyramidUsingNumbersWhereNumber=NumberOfTheRow.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout<<"Enter no. of rows:";
+    cin>>x;
+    for(int i=1;i<=x;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
